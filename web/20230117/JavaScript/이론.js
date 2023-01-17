250afdeab0ab5d82ec2b91033e16765f07e23492
pgcgoodi <goodi421@naver.com>
2023-01-17 17:33:01 +0900

    230117

---
+++ web/20230117/JavaScript/이론.js
@@ -0,0 +1,17 @@
+/* 변수 선언 시 선언자를 반듯 붙여라.
+   선언자는 총 세 종류가 있다.
+   var: 안씀, 가끔 코딩 선배들이 쓰는데 뭐라하진 마라.
+   let: 안씀.
+   const: 이걸 써라. 근데 만약 작동안되면 let 써라. */
+/* 자료형(typeof a)
+   원시 자료형: number(정수, 실수 모두 포함), string(문자 및 문자열/""사용), boolean(true, false) */
+/* 0: 수치로서 0
+   null: 개발자의 의도로 비워놓음
+   undefined: 개발자가 의도치 않았는데 비어있음 */
+/* 참조 자료형: array, object, function */
+/* 함수 선언할 때는 function 붙임
+   함수를 변수에 저장 가능 
+   const a = function (); */
+/* 객체 object{}: key와 value로 이루어진 property의 모음(key 중복 X) */
+/* 배열과 객체 각각의 요소는 const로 선언됐다고 하더라도 변경 가능 / 전체를 새로 선언하는 것은 불가능 */
+/* for, while에서 바뀌는 변수는 let으로 선언 */
